3a8360ab687b19145d02963f3af5fbf11046ec75
Ryan Plessner <plessner.ryan@gmail.com>
2014-10-16 21:47:04 -0400

    added a GeometryFactory that generates cubes and planes. Now the world shows a cube falling onto a plane

--- app/js/app/GeometryFactory.js
+++ app/js/app/GeometryFactory.js
@@ -3,17 +3,37 @@
  */
 define(['three', 'cannon'], function(Three, Cannon) {
 
+  Three.Mesh.prototype.updateRender = function() {
+    this.position.copy(this.body.position);
+    this.quaternion.copy(this.body.quaternion);
+  };
+
   var geometryFactory = function(scene, physics) {
     return {
-      cube: function(size, materialOptions, bodyOptions) {
-        var shape = new Cannon.Box(new Cannon.Vec3(size.x / 2, size.y / 2, size.z / 2));
-        var body = new Cannon.Body(bodyOptions);
+      plane: function(options) {
+        var shape = new Cannon.Plane();
+        var body = new Cannon.Body(options.physics);
+        body.addShape(shape);
+        body.quaternion.setFromAxisAngle(new Cannon.Vec3(1, 0, 0), -Math.PI / 2);
+        physics.add(body);
+
+        var size = options.size;
+        var geometry = new Three.PlaneGeometry(size.width, size.depth, 1, 1);
+        var plane = new Three.Mesh(geometry, new Three.MeshBasicMaterial(options.material));
+        scene.add(plane);
+        plane.body = body;
+        return plane;
+      },
+
+      cube: function(options) {
+        var size = options.size;
+        var shape = new Cannon.Box(new Cannon.Vec3(size.width / 2, size.height / 2, size.depth / 2));
+        var body = new Cannon.Body(options.physics);
         body.addShape(shape);
         physics.add(body);
 
-        var geometry = new Three.BoxGeometry(size.x, size.y, size.z);
-        var material = new Three.MeshBasicMaterial(materialOptions);
-        var mesh = new Three.Mesh(geometry, material);
+        var geometry = new Three.BoxGeometry(size.width, size.height, size.depth);
+        var mesh = new Three.Mesh(geometry, new Three.MeshBasicMaterial(options.material));
         scene.add(mesh);
         mesh.body = body;
         return mesh;
--- app/js/app/world.js
+++ app/js/app/world.js
@@ -1,13 +1,14 @@
 define(['three', 'cannon', 'GeometryFactory'],
        function(Three, Cannon, geoFactory) {
   var scene, renderer, physics, timeStep = 1/60;
-  var camera, box;
+  var camera, box, ground;
   var worldHelpers = {
 
     init: function() {
       scene = new Three.Scene();
-      camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 100);
-      camera.position.z = 5;
+      camera = new Three.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 1, 1000);
+      camera.position.set(10, 2, 0);
+      camera.quaternion.setFromAxisAngle(new THREE.Vector3(0,1,0), Math.PI/2);
       scene.add(camera);
 
       renderer = new Three.WebGLRenderer();
@@ -16,28 +17,34 @@ define(['three', 'cannon', 'GeometryFactory'],
       document.body.appendChild(renderer.domElement);
 
       physics = new Cannon.World();
-      physics.gravity.set(0,0,0);
+      physics.gravity.set(0, -10, 0);
       physics.broadphase = new Cannon.NaiveBroadphase();
       physics.solver.iterations = 10;
+      physics.solver.tolerance = 0.001;
 
       var worldBuilder = geoFactory(scene, physics);
-      box = worldBuilder.cube({x: 2, y: 2, z: 2}, { color: 0xff0000, wireframe: true }, { mass: 1});
-      box.body.angularVelocity.set(0,10,0);
-      box.body.angularDamping = 0.5;
+
+      ground = worldBuilder.plane({
+        size: { width: 100, depth: 100 },
+        material: { color: 0xff0000 },
+        physics: { mass: 0, material: new Cannon.Material() }
+      });
+      ground.body.quaternion.setFromAxisAngle(new Cannon.Vec3(1, 0, 0), -Math.PI / 2);
+      ground.updateRender();
+
+      box = worldBuilder.cube({
+        size: { width: 2, height: 3, depth: 2},
+        material: { color: 0x00ff00, wireframe: true },
+        physics: { mass: 5 }
+      });
+      box.body.position.set(0, 5, 0);
     },
 
     render: function() {
-      // note: three.js includes requestAnimationFrame shim
       requestAnimationFrame(worldHelpers.render);
 
       physics.step(timeStep);
-
-      // Copy coordinates from Cannon.js to Three.js
-      box.position.copy(box.body.position);
-      box.quaternion.copy(box.body.quaternion);
-
-      box.rotation.x += 0.01;
-      box.rotation.y += 0.02;
+      box.updateRender();
 
       renderer.render(scene, camera);
     }
